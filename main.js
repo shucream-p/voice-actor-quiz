b57427851e10c27beda71688d412e021cead6cc7
tominaga <aurora25sh.50nonono@gmail.com>
2022-06-30 23:20:07 +0900

    正解で10点加算し、最後に表示させる処理を追加

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ const { Quiz } = require('enquirer')
 const data = require('./data.js')
 
 class VoiceActorQuiz {
+  constructor () {
+    this.point = 0
+  }
+
   async start () {
     for (let i = 0; i < data.length; i++) {
       await this.giveQuiz(data[i])
     }
+    console.log(`結果は${this.point}点でした。`)
     console.log('お疲れ様でした。')
   }
 
@@ -14,6 +19,7 @@ class VoiceActorQuiz {
     const answer = await prompt.run()
     if (answer.correct) {
       console.log(`正解！ 担当声優は${prompt.actor}さんです。\n`)
+      this.point += 10
     } else {
       console.log(`不正解！ 正解は「${answer.correctAnswer}」です。担当声優は${prompt.actor}さんです。\n`)
     }
