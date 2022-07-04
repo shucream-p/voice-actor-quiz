df972760cc425c14b0b44b8a040d7da47cc8fa9b
tominaga <aurora25sh.50nonono@gmail.com>
2022-07-04 20:10:42 +0900

    データを増やした

--- data.js
+++ data.js
@@ -1,72 +1,281 @@
 module.exports.beginner = [{
   name: 'character',
-  message: '長門有希（涼宮ハルヒの憂鬱）と同じ声のキャラクターは次のうちどれ?\n\n  響け！ユーフォニアムより',
-  choices: ['田中あすか', '中世古香織', '小笠原晴香'],
-  correctChoice: 1,
-  actor: '茅原実里'
-},
-{
-  name: 'character',
-  message: '竈門禰󠄀豆子（鬼滅の刃）と同じ声のキャラクターは次のうちどれ?\n\n  ウマ娘プリティーダービーより',
-  choices: ['セイウンスカイ', 'ライスシャワー', 'メジロマックイーン'],
-  correctChoice: 0,
-  actor: '鬼頭明里'
-},
-{
-  name: 'character',
-  message: 'アルルゥ（うたわれるもの）と同じ声のキャラクターは次のうちどれ?\n\n  化物語より',
-  choices: ['戦場ヶ原ひたぎ', '神原駿河', '千石撫子'],
-  correctChoice: 1,
-  actor: '沢城みゆき'
-}]
-
-module.exports.intermediate = [{
-  name: 'character',
-  message: 'ギロロ伍長（ケロロ軍曹）と同じ声のキャラクターは次のうちどれ?\n\n  Fate/Zeroより',
-  choices: ['衛宮切嗣', '言峰綺礼', '遠坂時臣'],
-  correctChoice: 1,
-  actor: '中田譲治'
-},
-{
-  name: 'character',
-  message: '岡部倫太郎（STEINS;GATE(シュタインズ・ゲート)）と同じ声のキャラクターは次のうちどれ?\n\n  ちはやふるより',
-  choices: ['真島太一', '綿谷新', '周防久志'],
-  correctChoice: 0,
-  actor: '宮野真守'
-},
-{
-  name: 'character',
-  message: 'キリト（ソードアート・オンライン）と同じ声のキャラクターは次のうちどれ?\n\n  Re:ゼロから始める異世界生活より',
-  choices: ['レグルス・コルニアス', 'ライ・バテンカイトス', 'ペテルギウス・ロマネコンティ'],
-  correctChoice: 2,
-  actor: '松岡禎丞'
-}]
-
-module.exports.advenced = [{
-  name: 'character',
-  message: '円谷光彦（名探偵コナン）と同じ声のキャラクターは次のうちどれ?\n\n ポケットモンスターより',
+  message: '円谷光彦（名探偵コナン）と同じ声のキャラクターは次のうちどれ？\n\n 「ポケットモンスター」より',
   choices: ['ピカチュウ', 'トゲピー', 'チコリータ'],
   correctChoice: 0,
   actor: '大谷育江'
 },
 {
   name: 'character',
-  message: '野原ひろし（クレヨンしんちゃん）と同じ声のキャラクターは次のうちどれ?\n\n  鋼の錬金術師より',
+  message: '野原ひろし<初代>（クレヨンしんちゃん）と同じ声のキャラクターは次のうちどれ？\n\n  「鋼の錬金術師」より',
   choices: ['ロイ・マスタング', 'アレックス・ルイ・アームストロング', 'マース・ヒューズ'],
   correctChoice: 2,
   actor: '藤原啓治'
 },
 {
-  name: 'character',
-  message: '海馬瀬人（遊☆ 戯☆ 王デュエルモンスターズ）と同じ声のキャラクターは次のうちどれ?\n\n  呪術廻戦より',
-  choices: ['五条悟', '夏油傑', '七海建人'],
+  name: 'charactor',
+  message: '雪野百香里（言の葉の庭）と同じ声のキャラクターは次のうちどれ？\n\n  「鬼滅の刃」より',
+  choices: ['竈門禰󠄀豆子', '胡蝶しのぶ', '甘露寺蜜璃'],
   correctChoice: 2,
-  actor: '津田健次郎'
+  actor: '花澤香菜'
+},
+{
+  name: 'charactor',
+  message: 'ヨル・フォージャー（SPY × FAMILY）と同じ声のキャラクターは次のうちどれ？\n\n  「あの日見た花の名前を僕たちはまだ知らない。」より',
+  choices: ['本間芽衣子', '安城鳴子', '鶴見知利子'],
+  correctChoice: 2,
+  actor: '早見沙織'
+},
+{
+  name: 'charactor',
+  message: '両面宿儺(呪術廻戦)と同じ声のキャラクターは次のうちどれ？\n\n  「テニスの王子様」より',
+  choices: ['手塚国光', '乾貞治', '跡部景吾'],
+  correctChoice: 2,
+  actor: '諏訪部順一'
+},
+{
+  name: 'charactor',
+  message: 'ウソップ（ONE PIECE）と同じ声のキャラクターは次のうちどれ？\n\n  「DEATH NOTE」より',
+  choices: ['夜神月', 'L', 'ニア'],
+  correctChoice: 1,
+  actor: '山口勝平'
+},
+{
+  name: 'charactor',
+  message: '綾波レイ（新世紀エヴァンゲリオン）と同じ声のキャラクターは次のうちどれ？\n\n  「SHAMAN KING」より',
+  choices: ['恐山アンナ', '道潤', '玉村たまお'],
+  correctChoice: 0,
+  actor: '林原めぐみ'
+},
+{
+  name: 'charactor',
+  message: '草壁サツキ（となりのトトロ）と同じ声のキャラクターは次のうちどれ？\n\n  「らんま1/2」より',
+  choices: ['早乙女乱馬（女）', '天道あかね', 'シャンプー'],
+  correctChoice: 1,
+  actor: '日髙のり子'
+},
+{
+  name: 'charactor',
+  message: '三井寿（SLAM DUNK）と同じ声のキャラクターは次のうちどれ？\n\n  「BLEACH」より',
+  choices: ['浦原喜助', '阿散井恋次', '朽木白哉'],
+  correctChoice: 2,
+  actor: '置鮎龍太郎'
+},
+{
+  name: 'charactor',
+  message: 'エレン・イェーガー（進撃の巨人）と同じ声のキャラクターは次のうちどれ？\n\n  「僕のヒーローアカデミア」より',
+  choices: ['緑谷出久', '爆豪勝己', '轟焦凍'],
+  correctChoice: 2,
+  actor: '梶裕貴'
+}]
+
+module.exports.advenced = [{
+  name: 'character',
+  message: '長門有希（涼宮ハルヒの憂鬱）と同じ声のキャラクターは次のうちどれ？\n\n  「響け！ユーフォニアム」より',
+  choices: ['田中あすか', '中世古香織', '小笠原晴香'],
+  correctChoice: 1,
+  actor: '茅原実里'
 },
 {
   name: 'character',
-  message: '黒猫（俺の妹がこんなに可愛いわけがない。）と同じ声のキャラクターは次のうちどれ?\n\n  宇宙よりも遠い場所より',
-  choices: ['小淵沢報瀬', '三宅日向', '白石結月'],
+  message: '岡部倫太郎（STEINS;GATE(シュタインズ・ゲート)）と同じ声のキャラクターは次のうちどれ？\n\n  「ちはやふる」より',
+  choices: ['真島太一', '綿谷新', '周防久志'],
   correctChoice: 0,
-  actor: '花澤香菜'
+  actor: '宮野真守'
+},
+{
+  name: 'character',
+  message: 'キリト（ソードアート・オンライン）と同じ声のキャラクターは次のうちどれ？\n\n  「Re:ゼロから始める異世界生活」より',
+  choices: ['レグルス・コルニアス', 'ライ・バテンカイトス', 'ペテルギウス・ロマネコンティ'],
+  correctChoice: 2,
+  actor: '松岡禎丞'
+},
+{
+  name: 'character',
+  message: 'ギロロ伍長（ケロロ軍曹）と同じ声のキャラクターは次のうちどれ？\n\n  「Fate/Zero」より',
+  choices: ['衛宮切嗣', '言峰綺礼', '遠坂時臣'],
+  correctChoice: 1,
+  actor: '中田譲治'
+},
+{
+  name: 'character',
+  message: 'アルルゥ（うたわれるもの）と同じ声のキャラクターは次のうちどれ？\n\n  「化物語」より',
+  choices: ['戦場ヶ原ひたぎ', '神原駿河', '千石撫子'],
+  correctChoice: 1,
+  actor: '沢城みゆき'
+},
+{
+  name: 'charactor',
+  message: '枢木スザク（コードギアス 反逆のルルーシュ）と同じ声のキャラクターは次のうちどれ？\n\n  「おそ松さん」より',
+  choices: ['おそ松', 'カラ松', 'チョロ松'],
+  correctChoice: 0,
+  actor: '櫻井孝宏'
+},
+{
+  name: 'charactor',
+  message: '早乙女アルト（マクロスF（フロンティア））と同じ声のキャラクターは次のうちどれ？\n\n  「小林さんちのメイドラゴン」より',
+  choices: ['ファフニール', '滝谷真', '会田タケト'],
+  correctChoice: 1,
+  actor: '中村悠一'
+},
+{
+  name: 'charactor',
+  message: 'ヴィヴィ（Vivy -Fluorite Eye\'s Song-）と同じ声のキャラクターは次のうちどれ？\n\n  「青春ブタ野郎はバニーガール先輩の夢を見ない」より',
+  choices: ['桜島麻衣', '古賀朋絵', '双葉理央'],
+  correctChoice: 2,
+  actor: '種崎敦美'
+},
+{
+  name: 'charactor',
+  message: '澤村・スペンサー・英梨々（冴えない彼女の育てかた）と同じ声のキャラクターは次のうちどれ？\n\n  「ダンジョンに出会いを求めるのは間違っているだろうか」より',
+  choices: ['ヘスティア', 'アイズ・ヴァレンシュタイン', 'リリルカ・アーデ'],
+  correctChoice: 1,
+  actor: '大西沙織'
+},
+{
+  name: 'charactor',
+  message: 'キュウべえ（魔法少女まどか☆マギカ）と同じ声のキャラクターは次のうちどれ？\n\n  「らき☆ すた」より',
+  choices: ['泉こなた', '柊かがみ', '柊つかさ'],
+  correctChoice: 1,
+  actor: '加藤英美里'
+},
+{
+  name: 'charactor',
+  message: 'ボボボーボ・ボーボボ（ボボボーボ・ボーボボ）と同じ声のキャラクターは次のうちどれ？\n\n  「ヴァイオレット・エヴァーガーデン」より',
+  choices: ['クラウディア・ホッジンズ', 'ギルベルト・ブーゲンビリア', 'ディートフリート・ブーゲンビリア'],
+  correctChoice: 0,
+  actor: '子安武人'
+},
+{
+  name: 'charactor',
+  message: 'ジョセフ・ジョースター（ジョジョの奇妙な冒険）と同じ声のキャラクターは次のうちどれ？\n\n  「妖狐×僕SS」より',
+  choices: ['御狐神双熾', '反ノ塚連勝', '青鬼院蜻蛉'],
+  correctChoice: 2,
+  actor: '杉田智和'
+},
+{
+  name: 'charactor',
+  message: '藤林杏（CLANNAD -クラナド-）と同じ声のキャラクターは次のうちどれ？\n\n  「ARIA」より',
+  choices: ['水無灯里', '藍華・S・グランチェスタ', 'アリス・キャロル'],
+  correctChoice: 2,
+  actor: '広橋涼'
+},
+{
+  name: 'charactor',
+  message: '小鳥遊六花（中二病でも恋がしたい！）と同じ声のキャラクターは次のうちどれ？\n\n  「約束のネバーランド」より',
+  choices: ['エマ', 'ノーマン', 'レイ'],
+  correctChoice: 1,
+  actor: '内田真礼'
+},
+{
+  name: 'charactor',
+  message: '一方通行＜アクセラレータ＞（とある魔術の禁書目録）と同じ声のキャラクターは次のうちどれ？\n\n  「ハイキュー！」より',
+  choices: ['影山飛雄', '東峰旭', '西谷夕'],
+  correctChoice: 2,
+  actor: '岡本信彦'
+},
+{
+  name: 'charactor',
+  message: 'エウレカ（交響詩篇エウレカセブン）と同じ声のキャラクターは次のうちどれ？\n\n  「神のみぞ知るセカイ」より',
+  choices: ['エルシィ', '小坂ちひろ', '鮎川天理'],
+  correctChoice: 2,
+  actor: '名塚佳織'
+},
+{
+  name: 'charactor',
+  message: 'チト（少女終末旅行）と同じ声のキャラクターは次のうちどれ？\n\n  「宇宙よりも遠い場所」より',
+  choices: ['玉木マリ', '小淵沢報瀬', '三宅ひなた'],
+  correctChoice: 0,
+  actor: '水瀬いのり'
+},
+{
+  name: 'charactor',
+  message: '平沢唯（けいおん！）と同じ声のキャラクターは次のうちどれ？\n\n  「ココロコネクト」より',
+  choices: ['永瀬伊織', '稲葉姫子', '桐山唯'],
+  correctChoice: 0,
+  actor: '豊崎愛生'
+},
+{
+  name: 'charactor',
+  message: '志摩リン（ゆるキャン△）と同じ声のキャラクターは次のうちどれ？\n\n  「この美術部には問題がある！」より',
+  choices: ['宇佐美みずき', 'コレット', '伊万莉まりあ'],
+  correctChoice: 2,
+  actor: '東山奈央'
+},
+{
+  name: 'charactor',
+  message: '園田海未（ラブライブ！）と同じ声のキャラクターは次のうちどれ？\n\n  「少女☆ 歌劇 レヴュースタァライト」より',
+  choices: ['愛城華恋', '神楽ひかり', '大場なな'],
+  correctChoice: 1,
+  actor: '三森すずこ'
+},
+{
+  name: 'charactor',
+  message: '杉元佐一（ゴールデンカムイ）と同じ声のキャラクターは次のうちどれ？\n\n  「イエスタデイをうたって」より',
+  choices: ['魚住陸生', '早川浪', '湊'],
+  correctChoice: 0,
+  actor: '小林親弘'
+},
+{
+  name: 'charactor',
+  message: '八神コウ（NEW GAME!）と同じ声のキャラクターは次のうちどれ？\n\n  「ノーゲーム・ノーライフ」より',
+  choices: ['白', 'ステファニー・ドーラ', 'ジブリール'],
+  correctChoice: 1,
+  actor: '日笠陽子'
+},
+{
+  name: 'charactor',
+  message: '小戸川宏（オッドタクシー）と同じ声のキャラクターは次のうちどれ？\n\n  「四月は君の嘘」より',
+  choices: ['有馬公正', '渡亮太', '相座武士'],
+  correctChoice: 0,
+  actor: '花江夏樹'
+},
+{
+  name: 'charactor',
+  message: '音無結弦(Angel Beats!)と同じ声のキャラクターは次のうちどれ？\n\n  「進撃の巨人」より',
+  choices: ['エレン・イェーガー', 'コニー・スプリンガー', 'リヴァイ・アッカーマン'],
+  correctChoice: 2,
+  actor: '神谷浩史'
+},
+{
+  name: 'charactor',
+  message: '海馬瀬人（遊☆ 戯☆ 王デュエルモンスターズ）と同じ声のキャラクターは次のうちどれ？\n\n  「TIGGER & BUNNY」より',
+  choices: ['鏑木・T・虎徹（ワイルドタイガー）', 'ネイサン・シーモア（ファイヤーエンブレム）', 'キース・グッドマン（スカイハイ）'],
+  correctChoice: 1,
+  actor: '津田健次郎'
+},
+{
+  name: 'charactor',
+  message: '中野四葉（五等分の花嫁）と同じ声のキャラクターは次のうちどれ？\n\n  「やはり俺の青春ラブコメは間違っている。」より',
+  choices: ['雪ノ下雪乃', '由比ヶ浜結衣', '一色いろは'],
+  correctChoice: 2,
+  actor: '佐倉綾音'
+},
+{
+  name: 'charactor',
+  message: '桂小太郎（銀魂）と同じ声のキャラクターは次のうちどれ？\n\n  「最遊記」より',
+  choices: ['玄奘三蔵', '沙悟浄', '猪八戒'],
+  correctChoice: 2,
+  actor: '石田彰'
+},
+{
+  name: 'charactor',
+  message: '吉井明久（バカとテストと召喚獣）と同じ声のキャラクターは次のうちどれ？\n\n  「はたらく魔王さま！」より',
+  choices: ['真奥貞夫（サタン・ジャコブ）', '芦屋四郎（アルシエル）', '漆原半蔵（ルシフェル）'],
+  correctChoice: 2,
+  actor: '下野紘'
+},
+{
+  name: 'charactor',
+  message: '剛田猛男（俺物語）と同じ声のキャラクターは次のうちどれ？\n\n  「王様ランキング」より',
+  choices: ['ドーマス', 'ベビン', 'アピス'],
+  correctChoice: 0,
+  actor: '江口拓也'
+},
+{
+  name: 'charactor',
+  message: '羽入（ひぐらしのなく頃に）と同じ声のキャラクターは次のうちどれ？\n\n  「さくら荘のペットな彼女」より',
+  choices: ['椎名ましろ', '上井草美咲', '赤坂龍之介'],
+  correctChoice: 2,
+  actor: '堀江由衣'
 }]
