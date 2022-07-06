47d74699a045d3036aaf2cc55c82bc9efd29d65e
tominaga <aurora25sh.50nonono@gmail.com>
2022-07-06 10:54:33 +0900

    firstMessageの表示の修正と、条件によってlastMeesageを決めるメソッドを作成

--- main.js
+++ main.js
@@ -8,15 +8,17 @@ class VoiceActorQuiz {
   }
 
   async start () {
-    const firstMessage = '声優クイズを始めます！\n出題されるキャラクターの担当声優さんが、他に演じているキャラクターを当てるゲームです。\n'
+    const firstMessage = `声優クイズを始めます！
+出題されるキャラクターの担当声優さんが、他に演じているキャラクターを3択の中から選んでください。
+※「オタク向け」は10問以上用意してるので、是非、何回も遊んでみてね！
+`
     console.log(firstMessage)
     const quizzes = await this.#quizzesShuffle()
 
     for (let i = 0; i < 10; i++) {
       await this.#giveQuiz(quizzes[i])
     }
-    console.log(`結果は${this.point}点でした。`)
-    console.log('お疲れ様でした。')
+    this.showLastmessage()
   }
 
   async #quizzesShuffle () {
@@ -25,9 +27,9 @@ class VoiceActorQuiz {
   }
 
   async #getQuizzes () {
-    const level = await this.#selectLevel()
+    this.level = await this.#selectLevel()
 
-    if (level === '一般向け') {
+    if (this.level === '一般向け') {
       return data.beginner
     } else {
       return data.advenced
@@ -60,6 +62,26 @@ class VoiceActorQuiz {
     } else {
       console.log(`不正解！ 正解は「${answer.correctAnswer}」です。担当声優は${prompt.actor}さんです。\n`)
     }
+  }
+
+  showLastmessage () {
+    console.log(`結果は${this.point}点でした。`)
+    if (this.level === '一般向け') {
+      if (this.point >= 80) {
+        console.log('合格です！あなたはオタクの才能があります。是非、オタク向けにも挑戦してみてね！')
+      } else {
+        console.log('残念！不合格です〜。是非、また挑戦してみてね！')
+      }
+    } else {
+      if (this.point >= 80) {
+        console.log('合格です！かなり熱心にアニメを見ていますね！素晴らしい！是非、また遊んでみてね！')
+      } else if (this.point >= 50) {
+        console.log('惜しい！不合格です〜。しかし、なかなかに詳しいですね。是非、また挑戦してみてね！')
+      } else {
+        console.log('残念！不合格です〜。是非、また挑戦してみてね！')
+      }
+    }
+    console.log('お疲れ様でした。')
   }
 }
 
